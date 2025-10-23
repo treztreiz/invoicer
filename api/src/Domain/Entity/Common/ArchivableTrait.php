d576aa3ff9eda26f36ae9ec937087bcda1dd0fde
treztreiz <mathias@wuaro.com>
2025-10-23 23:31:17 +0400

    refactor: export archive property to trait

--- api/src/Domain/Entity/Common/ArchivableTrait.php
+++ api/src/Domain/Entity/Common/ArchivableTrait.php
@@ -2,6 +2,24 @@
 
 namespace App\Domain\Entity\Common;
 
+use Doctrine\ORM\Mapping as ORM;
+
 trait ArchivableTrait
 {
+    #[ORM\Column]
+    private bool $archived = false;
+
+    // /////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    public function isArchived(): bool
+    {
+        return $this->archived;
+    }
+
+    public function setArchived(bool $archived): static
+    {
+        $this->archived = $archived;
+
+        return $this;
+    }
 }
--- api/src/Domain/Entity/Customer.php
+++ api/src/Domain/Entity/Customer.php
@@ -2,19 +2,20 @@
 
 namespace App\Domain\Entity;
 
+use App\Domain\Entity\Common\ArchivableTrait;
 use App\Domain\Entity\Common\TimestampableTrait;
 use App\Domain\Entity\Common\UuidTrait;
 use App\Domain\ValueObject\Address;
 use App\Domain\ValueObject\Contact;
 use App\Domain\ValueObject\Name;
 use Doctrine\ORM\Mapping as ORM;
-use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity]
 class Customer
 {
     use UuidTrait;
     use TimestampableTrait;
+    use ArchivableTrait;
 
     public function __construct(
         #[ORM\Embedded(columnPrefix: false)]
@@ -25,10 +26,6 @@ class Customer
 
         #[ORM\Embedded]
         private Address $address,
-
-        #[ORM\Column]
-        #[Assert\Type(type: ['bool'])]
-        private bool $archived = false,
     ) {
     }
 
@@ -66,18 +63,6 @@ class Customer
     public function setAddress(Address $address): static
     {
         $this->address = $address;
-
-        return $this;
-    }
-
-    public function isArchived(): bool
-    {
-        return $this->archived;
-    }
-
-    public function setArchived(bool $archived): static
-    {
-        $this->archived = $archived;
 
         return $this;
     }
