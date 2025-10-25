29c738588d72b5c8232982d462f37b352be53ddf
treztreiz <mathias@wuaro.com>
2025-10-25 14:46:46 +0400

    feature: add number sequence aggregate

---
+++ api/src/Domain/Entity/Numbering/NumberSequence.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Domain\Entity\Numbering;
+
+use App\Domain\Entity\Common\UuidTrait;
+use App\Domain\Enum\DocumentType;
+use App\Domain\Guard\DomainGuard;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity]
+#[ORM\Table(name: 'number_sequence')]
+#[ORM\UniqueConstraint(columns: ['document_type', 'year'])]
+class NumberSequence
+{
+    use UuidTrait;
+
+    public function __construct(
+        #[ORM\Column(enumType: DocumentType::class)]
+        private(set) readonly DocumentType $documentType,
+
+        #[ORM\Column(type: Types::SMALLINT)]
+        private(set) int $year {
+            set => DomainGuard::nonNegativeInt($value, 'Year');
+        },
+
+        #[ORM\Column(type: Types::BIGINT)]
+        private(set) int $nextValue = 1,
+    ) {
+    }
+
+    public function reserveNext(): int
+    {
+        $current = $this->nextValue;
+        $this->nextValue = DomainGuard::nonNegativeInt($this->nextValue + 1, 'Next value');
+
+        return $current;
+    }
+}
